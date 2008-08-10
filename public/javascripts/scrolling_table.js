63d04f515a44155e7c5324030987146184e9c798
sw <sw@2d86388d-c40f-0410-ad6a-a69da6a65d20>
2008-08-10 19:27:21 +0000

    Snapshot working revert effect
    git-svn-id: http://butlerpress.com/var/repos/racing_on_rails/branches/admin_ui@1383 2d86388d-c40f-0410-ad6a-a69da6a65d20

--- public/javascripts/scrolling_table.js
+++ public/javascripts/scrolling_table.js
@@ -40,7 +40,10 @@ YAHOO.extend(ScrollingTableDDProxy, YAHOO.util.DDProxy, {
         var s  = div.style;
         s.position   = "absolute";
         s.visibility = "hidden";
+        // TODO use setStyle('opacity')
         s.opacity    = "0.50";
+        Dom.setStyle(div, '-moz-opacity', '0.50');
+        Dom.setStyle(div, 'filter', 'alpha(opacity=50)');        
         s.cursor     = "default";
         s.zIndex     = 999;
         s.height     = "auto";
@@ -102,46 +105,41 @@ YAHOO.extend(ScrollingTableDDProxy, YAHOO.util.DDProxy, {
       var DOM = YAHOO.util.Dom;
       var lel = this.getEl();
       var del = this.getDragEl();
-
-      // Show the drag frame briefly so we can get its position
+      // 
+      // // Show the drag frame briefly so we can get its position
       DOM.setStyle(del, "visibility", ""); 
-
-      // Hide the linked element before the move to get around a Safari 
-      // rendering bug.
-      DOM.setStyle(lel, "visibility", "hidden"); 
-      DOM.setStyle(lel, "visibility", ""); 
-
+      // 
+      // // Hide the linked element before the move to get around a Safari 
+      // // rendering bug.
+      // DOM.setStyle(lel, "visibility", "hidden"); 
+      // DOM.setStyle(lel, "visibility", ""); 
+      // 
       left_offset = DOM.getX(del) - DOM.getX(lel);
       top_offset = DOM.getY(del) - DOM.getY(lel);
       var dur = Math.sqrt(Math.abs(top_offset^2)+Math.abs(left_offset^2))*0.02;
-      new Effect.Move(del, { x: -left_offset, y: -top_offset, duration: dur,
-        queue: {scope:'_draggable', position:'end'}
-      });
-      new Effect.Opacity(del, {duration:0.2, from:0.5, to:0.0, 
-        queue: {scope:'_draggable', position:'end'}
-      });
+      var dragElementClone = document.createElement("div");
+      dragElementClone.style.width = '200px';
+      dragElementClone.style.height = '20px';
+      dragElementClone.style.backgroundColor = 'red';
+      dragElementClone.style.zIndex = 999;
+      dragElementClone.style.position = "absolute";
+      
+      document.body.insertBefore(dragElementClone, document.body.firstChild);
+      new YAHOO.util.Motion(dragElementClone, {points: { from: [DOM.getX(del), DOM.getY(del)], to: [DOM.getX(lel), DOM.getY(lel)] } }, 1, YAHOO.util.Easing.easeOut).animate();
+      new YAHOO.util.Anim(dragElementClone, { 
+                                              opacity: { to: 0.0 }, 
+                                              zIndex: { to: -1 },
+                                              left: { from: DOM.getX(del), to: DOM.getX(lel) }, 
+                                              top: { from: DOM.getY(del), to: DOM.getY(lel) }
+                                            }).animate();
+      YAHOO.util.DDM.moveToEl(lel, del);
+      DOM.setStyle(del, "visibility", "hidden"); 
+      // new Effect.Move(del, { x: -left_offset, y: -top_offset, duration: dur});
+      // new Effect.Opacity(del, {duration:0.2, from:0.5, to:0.0});
+      // new Effect.Morph(del, {style: 'visibility: hidden; opacity: 0.5;', duration: 0.1});
+      // new Effect.Move(del, { y: -100, duration: 0.1});
   }
 });
-
-function add_droppable_row_for(recordId) {
-  // YAHOO.util.Event.onDOMReady(function() { 
-  //   new YAHOO.util.DDTarget('team_' + recordId + "_name");
-  // });
-  // Droppables.add('team_' + recordId + '_name', 
-  //                { hoverclass:'hovering', 
-  //                  onDrop:function(element) {
-  //                    console.log('Dropped ' + idRegex.exec(element.id) + ' on ' + recordId);
-  //                    // Droppables.remove('team_' + recordId + '_row');
-  //                    // $('team_' + recordId).addClassName("disabled");
-  //                    // new Effect.Opacity('team_' + recordId + '_row', { from: 1.0, to: 0.5, duration: 0.5 });
-  //                    // new Ajax.Request('/admin/teams/merge?target_id=' + recordId, 
-  //                    //                  { asynchronous:true, 
-  //                    //                    evalScripts:true, 
-  //                    //                    parameters:'id=' + encodeURIComponent(idRegex.exec(element.id))
-  //                    //                   }
-  //                    //                  );
-  //                    }})  
-}
 
 function add_draggable_for(recordId, recordName) {
     YAHOO.util.Event.onDOMReady(function() { 
@@ -247,23 +245,25 @@ function add_draggable_for(recordId, recordName) {
         y = YAHOO.util.Event.getPageY(e);
         recordsX = YAHOO.util.Dom.getX("records");
         recordsY = YAHOO.util.Dom.getY("records");
-        if (y < recordsY) {
-          alert("Above table");
+        if (y < recordsY || (y > recordsY + recordsDiv.getHeight())) {
+          return;
         }
-        if (y > recordsY + recordsDiv.getHeight()) {
-          alert("below table");
-        }
-      }    
+        
+        YAHOO.util.DragDropMgr.lock();
+        YAHOO.util.Dom.removeClass(id, "draggingOver");
+        
+        $(id).addClassName("disabled");
+        new Effect.Opacity(id, { from: 1.0, to: 0.5, duration: 0.5 });
+        new Effect.Opacity(this.getEl().id, { from: 1.0, to: 0.5, duration: 0.5 });
+        new Ajax.Request('/admin/teams/merge?target_id=' + idRegex.exec(this.getEl().id), 
+                         { asynchronous:true, 
+                           evalScripts:true, 
+                           parameters:'id=' + encodeURIComponent(idRegex.exec(id))
+                          }
+                         );
+
+      }
     });
-  // new Draggable('team_' + recordId + '_name', { delay:100,
-  //                                      scroll: $('records'),
-  //                                      // ghosting: !Prototype.Browser.IE,
-  //                                      superghosting: true,
-  //                                      revert: true,
-  //                                      onEnd:function() { add_droppable_row_for(recordId); },
-  //                                      onStart:function() { Droppables.remove('team_' + recordId + '_name'); dragging = true;}
-  //                                    }
-  //               );
 }
 
 function clicked(e) {
